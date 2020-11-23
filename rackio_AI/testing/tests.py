05f05dab692267cc6d4561b101e880bc46dc8ae1
crivero7 <cdrr.rivero@gmail.com>
2020-11-23 08:32:21 -0400

    Small changes in tests

--- rackio_AI/testing/tests.py
+++ rackio_AI/testing/tests.py
@@ -7,6 +7,7 @@ from rackio_AI.preprocessing import Preprocessing
 from rackio_AI.data import get_directory
 from rackio import Rackio
 
+
 class LoadDataTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -27,7 +28,7 @@ class LoadDataTestCase(unittest.TestCase):
         :return:
         """
         RackioAI(self.app)
-        self.assertTrue(isinstance(RackioAI.load(self.tpl_filename), pd.DataFrame)==True)
+        self.assertTrue(isinstance(RackioAI.load(self.tpl_filename), pd.DataFrame) == True)
 
     def testLoadDataFrameInPickle(self):
         """
@@ -35,7 +36,7 @@ class LoadDataTestCase(unittest.TestCase):
         :return:
         """
         RackioAI(self.app)
-        self.assertTrue(isinstance(RackioAI.load(self.pkl_filename), pd.DataFrame)==True)
+        self.assertTrue(isinstance(RackioAI.load(self.pkl_filename), pd.DataFrame) == True)
 
     def testLoadFileNotFound(self):
         """
@@ -73,24 +74,26 @@ class SplitterTestCase(unittest.TestCase):
         :return:
         """
         RackioAI(self.app)
-        preprocess = Preprocessing(name='Preprocess model name', description='preprocess for data', problem_type='regression')
+        preprocess = Preprocessing(name='Preprocess model name', description='preprocess for data',
+                                   problem_type='regression')
         X, y = np.arange(20000).reshape((10000, 2)), range(10000)
         # Number of test to do
         number_of_tests = 100
         for i in range(number_of_tests):
-
             # Generate amount of parameter to pass as function argument, if rand_int=1, you pass only train_size parameter
             # rand_int=2, you pass train_size and test_size parameters
             # rand_int=3, you pass train_size, test_size and validation size parameters
-            rand_int = np.random.randint(3)+1
+            rand_int = np.random.randint(3) + 1
             # Generate proportion split for train_test_validation parameters
             array = np.random.random(rand_int)
             # Parameter definition
             keys = ['train_size', 'test_size', 'validation_size']
             # Create dictionary of normalized parameter
-            train_test_validation_size = {'{}'.format(keys[count]): value / np.sum(array) if rand_int > 1 else value for count, value in enumerate(array)}
+            train_test_validation_size = {'{}'.format(keys[count]): value / np.sum(array) if rand_int > 1 else value for
+                                          count, value in enumerate(array)}
             # Do the test
             self.assertIsInstance(preprocess.splitter.split(X, y, random_state=0, **train_test_validation_size), list)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     unittest.main()
