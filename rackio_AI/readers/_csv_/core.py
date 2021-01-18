3160108fc6d89787da25638fce80ff52f83e9934
crivero7 <cdrr.rivero@gmail.com>
2021-01-18 18:49:41 -0400

    Changes in csv documentation

--- rackio_AI/readers/_csv_/core.py
+++ rackio_AI/readers/_csv_/core.py
@@ -27,7 +27,7 @@ class CSV:
 
         **Parameters**
 
-        * **:param csv_files:** * **(str, path object or file-like object)** * 
+        * **:param csv_files: (str, path object or file-like object)** 
 
         Any valid string path is acceptable. The string could be a URL. Valid URL
         schemes include http, ftp, s3, gs, and file. For file URLs, a host is expected.
@@ -37,7 +37,7 @@ class CSV:
         By file-like object, we refer to objects with a read() method, such as a file handle (e.g. via builtin
         open function) or StringIO.
 
-        * **:param sep:** * **(str, default ‘,’)** *
+        * **:param sep: (str, default ‘,’)**
 
         Delimiter to use. If sep is None, the C engine cannot automatically detect the separator, but the Python
         parsing engine can, meaning the latter will be used and automatically detect the separator by Python’s 
@@ -45,11 +45,11 @@ class CSV:
         '\s+' will be interpreted as regular expressions and will also force the use of the Python parsing 
         engine. Note that regex delimiters are prone to ignoring quoted data. Regex example: '\r\t'.
 
-        * **:param delimiter:** * **(str, default None)** *
+        * **:param delimiter: (str, default None)**
 
         Alias for sep.
 
-        * **:param header:** * **(int, list of int, default ‘infer’)** *
+        * **:param header: (int, list of int, default ‘infer’)**
 
         Row number(s) to use as the column names, and the start of the data. Default behavior is to infer the 
         column names: if no names are passed the behavior is identical to header=0 and column names are inferred 
@@ -60,12 +60,12 @@ class CSV:
         commented lines and empty lines if skip_blank_lines=True, so header=0 denotes the first line of data 
         rather than the first line of the file.
 
-        * **:param names:** * **(array-like, optional)** *
+        * **:param names: (array-like, optional)**
 
         List of column names to use. If the file contains a header row, then you should explicitly pass header=0 
         to override the column names. Duplicates in this list are not allowed.
 
-        * **:param index_col:** * **(int, str, sequence of int / str, or False, default None)** *
+        * **:param index_col: (int, str, sequence of int / str, or False, default None)**
 
         Column(s) to use as the row labels of the DataFrame, either given as string name or column index. If a
         sequence of int / str is given, a MultiIndex is used.
@@ -73,7 +73,7 @@ class CSV:
         Note: index_col=False can be used to force pandas to not use the first column as the index, e.g. when 
         you have a malformed file with delimiters at the end of each line.
 
-        * **:param usecols:** * **(list-like or callable, optional)** *
+        * **:param usecols: (list-like or callable, optional)**
 
         Return a subset of the columns. If list-like, all elements must either be positional (i.e. integer indices
         into the document columns) or strings that correspond to column names provided either by the user in names
@@ -88,46 +88,46 @@ class CSV:
         lambda x: x.upper() in ['AAA', 'BBB', 'DDD']. Using this parameter results in much faster parsing time and
         lower memory usage.
 
-        * **:param squeeze:** * **(bool, default False)** *
+        * **:param squeeze: (bool, default False)**
 
         If the parsed data only contains one column then return a Series.
 
-        * **:param prefix:** * **(str, optional)** *
+        * **:param prefix: (str, optional)**
 
         Prefix to add to column numbers when no header, e.g. ‘X’ for X0, X1, …
 
-        * **:param mangle_dupe_cols:** * **(bool, default True)** *
+        * **:param mangle_dupe_cols: (bool, default True)**
 
         Duplicate columns will be specified as ‘X’, ‘X.1’, …’X.N’, rather than ‘X’…’X’. Passing in False will cause 
         data to be overwritten if there are duplicate names in the columns.
 
-        * **:param dtype:** * **(Type name or dict of column -> type, optional)** *
+        * **:param dtype: (Type name or dict of column -> type, optional)**
 
         Data type for data or columns. E.g. {‘a’: np.float64, ‘b’: np.int32, ‘c’: ‘Int64’} Use str or object 
         together with suitable na_values settings to preserve and not interpret dtype. If converters are specified,
         they will be applied INSTEAD of dtype conversion.
 
-        * **:param engine:** * **({‘c’, ‘python’}, optional)** *
+        * **:param engine: ({‘c’, ‘python’}, optional)**
 
         Parser engine to use. The C engine is faster while the python engine is currently more feature-complete.
 
-        * **:param converters:** * **(dict, optional)** *
+        * **:param converters: (dict, optional)**
 
         Dict of functions for converting values in certain columns. Keys can either be integers or column labels.
 
-        * **:param true_values:** * **(list, optional)** *
+        * **:param true_values: (list, optional)**
 
         Values to consider as True.
 
-        * **:param false_values:** * **(list, optional)** *
+        * **:param false_values: (list, optional)**
 
         Values to consider as False.
 
-        * **:param skipinitialspace:** * **(bool, default False)** *
+        * **:param skipinitialspace: (bool, default False)**
 
         Skip spaces after delimiter.
 
-        * **:param skiprows:** * **(list-like, int or callable, optional)** *
+        * **:param skiprows: (list-like, int or callable, optional)**
 
         Line numbers to skip (0-indexed) or number of lines to skip (int) at the start of the file.
 
@@ -135,22 +135,22 @@ class CSV:
         True if the row should be skipped and False otherwise. An example of a valid callable argument
         would be lambda x: x in [0, 2].
 
-        * **:param skipfooter:** * **(int, default 0)** *
+        * **:param skipfooter: (int, default 0)**
 
         Number of lines at bottom of file to skip (Unsupported with engine=’c’).
 
-        * **:param nrows:** * **(int, optional)** *
+        * **:param nrows: (int, optional)**
 
         Number of rows of file to read. Useful for reading pieces of large files.
 
-        * **:param na_values:** * **(scalar, str, list-like, or dict, optional)** *
+        * **:param na_values: (scalar, str, list-like, or dict, optional)**
 
         Additional strings to recognize as NA/NaN. If dict passed, specific per-column NA values. 
         By default the following values are interpreted as NaN: ‘’, ‘#N/A’, ‘#N/A N/A’, ‘#NA’,
         ‘-1.#IND’, ‘-1.#QNAN’, ‘-NaN’, ‘-nan’, ‘1.#IND’, ‘1.#QNAN’, ‘<NA>’, ‘N/A’, ‘NA’, ‘NULL’, 
         ‘NaN’, ‘n/a’, ‘nan’, ‘null’.
 
-        * **:param keep_default_na:** * **(bool, default True)** *
+        * **:param keep_default_na: (bool, default True)**
 
         Whether or not to include the default NaN values when parsing the data. 
         Depending on whether na_values is passed in, the behavior is as follows:
@@ -165,20 +165,20 @@ class CSV:
 
         **Note** that if na_filter is passed in as False, the keep_default_na and na_values parameters will be ignored.
 
-        * **:param na_filter:** * **(bool, default True)** *
+        * **:param na_filter: (bool, default True)**
 
         Detect missing value markers (empty strings and the value of na_values). In data without any NAs, passing 
         na_filter=False can improve the performance of reading a large file.
 
-        * **:param verbose:** * **(bool, default False)** *
+        * **:param verbose: (bool, default False)**
 
         Indicate number of NA values placed in non-numeric columns.
 
-        * **:param skip_blank_lines:** * **(bool, default True)** *
+        * **:param skip_blank_lines: (bool, default True)**
 
         If True, skip over blank lines rather than interpreting as NaN values.
 
-        * **:param parse_dates:** * **(bool or list of int or names or list of lists or dict, default False)** *
+        * **:param parse_dates: (bool or list of int or names or list of lists or dict, default False)**
 
         The behavior is as follows:
 
@@ -195,17 +195,17 @@ class CSV:
 
         **Note:** A fast-path exists for iso8601-formatted dates.
 
-        * **:param infer_datetime_format:** * **(bool, default False)** *
+        * **:param infer_datetime_format: (bool, default False)**
 
         If True and parse_dates is enabled, pandas will attempt to infer the format of the datetime strings
         in the columns, and if it can be inferred, switch to a faster method of parsing them. In some cases
         this can increase the parsing speed by 5-10x.
 
-        * **:param keep_date_col:** * **(bool, default False)** *
+        * **:param keep_date_col: (bool, default False)**
 
         If True and parse_dates specifies combining multiple columns then keep the original columns.
 
-        * **:param date_parser:** * **(function, optional)** *
+        * **:param date_parser: (function, optional)**
 
         Function to use for converting a sequence of string columns to an array of datetime instances. 
         The default uses dateutil.parser.parser to do the conversion. Pandas will try to call date_parser in 
@@ -214,57 +214,57 @@ class CSV:
         defined by parse_dates into a single array and pass that; and 3) call date_parser once for each row using 
         one or more strings (corresponding to the columns defined by parse_dates) as arguments.
 
-        * **:param dayfirst:** * **(bool, default False)** *
+        * **:param dayfirst: (bool, default False)**
 
         DD/MM format dates, international and European format.
 
-        * **:param cache_dates:** * **(bool, default True)** *
+        * **:param cache_dates: (bool, default True)**
 
         If True, use a cache of unique, converted dates to apply the datetime conversion. May produce significant
         speed-up when parsing duplicate date strings, especially ones with timezone offsets.
 
-        * **:param iterator:** * **(bool, default False)** *
+        * **:param iterator: (bool, default False)**
 
         Return TextFileReader object for iteration or getting chunks with get_chunk().
 
-        * **:param chunksize:** * **(int, optional)** *
+        * **:param chunksize: (int, optional)**
 
         Return TextFileReader object for iteration. See the IO Tools docs for more information on iterator 
         and chunksize.
 
-        * **:param compression:** * **({‘infer’, ‘gzip’, ‘bz2’, ‘zip’, ‘xz’, None}, default ‘infer’)** *
+        * **:param compression: ({‘infer’, ‘gzip’, ‘bz2’, ‘zip’, ‘xz’, None}, default ‘infer’)**
 
         For on-the-fly decompression of on-disk data. If ‘infer’ and filepath_or_buffer is path-like, then detect
         compression from the following extensions: ‘.gz’, ‘.bz2’, ‘.zip’, or ‘.xz’ (otherwise no decompression). 
         If using ‘zip’, the ZIP file must contain only one data file to be read in. Set to None for no decompression.
 
-        * **:param thousands:** * **(str, optional)** *
+        * **:param thousands: (str, optional)**
 
         Thousands separator.
 
-        * **:param decimal:** * **(str, default ‘.’)** *
+        * **:param decimal: (str, default ‘.’)**
 
         Character to recognize as decimal point (e.g. use ‘,’ for European data).
 
-        * **:param lineterminator:** * **(str (length 1), optional)** *
+        * **:param lineterminator: (str (length 1), optional)**
 
         Character to break file into lines. Only valid with C parser.
 
-        * **:param quoting:** * **(int or csv.QUOTE_* instance, default 0)** *
+        * **:param quoting: (int or csv.QUOTE_* instance, default 0)**
 
         Control field quoting behavior per csv.QUOTE_* constants. Use one of QUOTE_MINIMAL (0), QUOTE_ALL (1), 
         QUOTE_NONNUMERIC (2) or QUOTE_NONE (3)
 
-        * **:param doublequote:** * **(bool, default True)** *
+        * **:param doublequote: (bool, default True)**
 
         When quotechar is specified and quoting is not QUOTE_NONE, indicate whether or not to interpret two consecutive
         quotechar elements INSIDE a field as a single quotechar element.
 
-        * **:param escapechar:** * **(str (length 1), optional)** *
+        * **:param escapechar: (str (length 1), optional)**
 
         One-character string used to escape other characters.
 
-        * **:param comment:** * **(str, optional)** *
+        * **:param comment: (str, optional)**
 
         Indicates remainder of line should not be parsed. If found at the beginning of a line, the line will be 
         ignored altogether. This parameter must be a single character. Like empty lines 
@@ -272,53 +272,49 @@ class CSV:
         skiprows. For example, if comment='#', parsing #empty\na,b,c\n1,2,3 with header=0 will result in ‘a,b,c’ 
         being treated as the header.
 
-        * **:param encoding:** * **(str, optional)** *
+        * **:param encoding: (str, optional)**
 
         Encoding to use for UTF when reading/writing (ex. ‘utf-8’). List of Python standard encodings .
 
-        * **:param dialect:** * **(str or csv.Dialect, optional)** *
+        * **:param dialect: (str or csv.Dialect, optional)**
 
         If provided, this parameter will override values (default or not) for the following parameters: 
         delimiter, doublequote, escapechar, skipinitialspace, quotechar, and quoting. If it is necessary to 
         override values, a ParserWarning will be issued. See csv.Dialect documentation for more details.
 
-        * **:param error_bad_lines:** * **(bool, default True)** *
+        * **:param error_bad_lines: (bool, default True)**
 
         Lines with too many fields (e.g. a csv line with too many commas) will by default cause an exception 
         to be raised, and no DataFrame will be returned. If False, then these “bad lines” will dropped from 
         the DataFrame that is returned.
 
-        * **:param warn_bad_lines:** * **(bool, default True)** *
+        * **:param warn_bad_lines: (bool, default True)**
 
         If error_bad_lines is False, and warn_bad_lines is True, a warning for each “bad line” will be output.
 
-        * **:param delim_whitespace:** * **(bool, default False)** *
+        * **:param delim_whitespace: (bool, default False)**
 
         Specifies whether or not whitespace (e.g. ' ' or '    ') will be used as the sep. Equivalent to 
         setting sep='\s+'. If this option is set to True, nothing should be passed in for the delimiter parameter.
 
-        * **:param low_memory:** * **(bool, default True)** *
+        * **:param low_memory: (bool, default True)**
 
         Internally process the file in chunks, resulting in lower memory use while parsing, but possibly 
         mixed type inference. To ensure no mixed types either set False, or specify the type with the dtype 
         parameter. Note that the entire file is read into a single DataFrame regardless, use the chunksize 
         or iterator parameter to return the data in chunks. (Only valid with C parser).
 
-        * **:param memory_map:** * **(bool, default False)** *
+        * **:param memory_map: (bool, default False)**
 
         If a filepath is provided for filepath_or_buffer, map the file object directly onto memory and access 
         the data directly from there. Using this option can improve performance because there is no longer 
         any I/O overhead.
 
-        * **:param float_precision:** * **(str, optional)** *
-
-        Specifies which converter the C engine should use for floating-point values. The options are None 
+        * **:param float_precision: (str, optional)** \nSpecifies which converter the C engine should use for floating-point values. The options are None 
         or ‘high’ for the ordinary converter, ‘legacy’ for the original lower precision pandas converter, 
         and ‘round_trip’ for the round-trip converter.
 
-        * **:param storage_options:** * **(dict, optional)** *
-
-        Extra options that make sense for a particular storage connection, e.g. host, port, username, 
+        * **:param storage_options: (dict, optional)** \nExtra options that make sense for a particular storage connection, e.g. host, port, username, 
         password, etc., if using a URL that will be parsed by fsspec, e.g., starting “s3://”, “gcs://”. 
         An error will be raised if providing this argument with a non-fsspec URL. See the fsspec and backend 
         storage implementation docs for the set of allowed keys and values.
