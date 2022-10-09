daacff7dc477a422de7b8cc6eae3bb4ce36a5ce0
JesusDBS <jesusbingucv@gmail.com>
2022-10-09 17:37:54 -0400

    [EDIT] tpl_core read genkey 98% done

--- rackio_AI/readers/tpl/tpl_core.py
+++ rackio_AI/readers/tpl/tpl_core.py
@@ -609,6 +609,144 @@ class Genkey(dict):
 
     #         self.__clean_last_key()
 
+    def clean_lines(self, lines: str):
+        '''
+        Documentation here
+        '''
+        # Append lines when it has \\
+        _el = ''
+        broken_lines = []
+        for el in lines.split('\n'):
+            if re.search('\\\\', el):
+                if not _el:
+                    _el = el
+                    continue
+                _el += el
+                continue
+
+            if el.find('\\\\') == -1 and _el and bool(el.strip()):
+                _el += el
+                _el = ' '.join([e.strip() for e in _el.split('\\')])
+                broken_lines.append(_el.strip())
+                _el = ''
+                continue
+
+            if bool(el.strip()):
+                broken_lines.append(el.strip())
+
+        # Append lines when it starts with third level key
+        _el = ''
+        fixed_lines = []
+        second_key_pattern = re.compile(r'^[a-zA-Z]+\s\w+')
+        third_key_pattern = re.compile(r'^[a-zA-Z]+\=|^[a-zA-Z]+\s\=')
+
+        for line in broken_lines:
+            if second_key_pattern.search(line):
+                _el = line
+                fixed_lines.append(line)
+                continue
+
+            if third_key_pattern.search(line):
+                line = ' ' + line
+                _el += line
+                fixed_lines.append(_el)
+                continue
+
+        return fixed_lines
+
+    def split_values(self, line):
+        '''
+        Documentation here
+        '''
+        _info = ''
+        _el = ''
+        clean_line = []
+        flag = False
+        second_key_pattern = re.compile(r'^[A-Z]+\s')
+        opening_third_key_pattern = re.compile(r'^[A-Z]+\=\(')
+        third_key_pattern = re.compile(r'^[A-Z]+\=')
+        closing_third_key_pattern = re.compile(r'\)$|\)\s.+$')
+
+        for el in line.split(', '):
+            if second_key_pattern.search(el):
+                splited_line = el.split(' ')
+                clean_line.append(splited_line[0])
+
+                if opening_third_key_pattern.search(splited_line[1]):
+                    _el = ' '.join([e for e in splited_line[1:]])
+                    flag = True
+                    continue
+                el = ' '.join([e for e in splited_line[1:]])
+                clean_line.append(el)
+                continue
+
+            if opening_third_key_pattern.search(el):
+                _el = el
+                flag = True
+                continue
+
+            if third_key_pattern.search(el):
+                if re.search(r'^INFO', el):
+                    _info = el
+                    continue
+
+                clean_line.append(el)
+                continue
+
+            if re.search(r'^INFO', _info):
+                _info = _info + ', ' + el
+                clean_line.append(_info)
+                _info = ''
+                continue
+
+            if flag:
+                el = ', ' + el
+                _el += el
+
+                if closing_third_key_pattern.search(el):
+                    clean_line.append(_el)
+                    _el = ''
+                    flag = False
+
+        return clean_line
+
+    def get_dict_values(self, values: list):
+        '''
+        Documentation here
+        '''
+        k = [el.split('=')[0] for el in values]
+        v = [el.split('=')[1] for el in values]
+        k_v = dict(zip(k, v))
+
+        pattern = re.compile(r'\d\s\w|\d\)\s\w+|\d\)\s\%|\d\s\%')
+        for key, val in k_v.items():
+            if re.search(r'\(\"\.\./', k_v[key]):
+                continue
+
+            if re.search(r'^INFO', key):
+                continue
+
+            if pattern.search(val):
+                val = val.split(' ')
+                VALUE = ' '.join([el for el in val[:-1]])
+                UNIT = val[-1]
+                plural = False
+
+                try:
+                    VALUE = eval(VALUE)
+                except:
+                    pass
+
+                if isinstance(VALUE, tuple):
+                    plural = True
+
+                k_v[key] = {
+                    f'VALUE{"S" if plural else ""}': VALUE,
+                    'UNIT': UNIT.strip(',')
+                }
+
+        return k_v
+
     def read(self, filename: str):
         '''
         Documentation here
@@ -628,26 +766,48 @@ class Genkey(dict):
 
         # Getting first level and second level Genkey keys
         first_level_key_pattern = re.compile('!\s\w+.+')
-        second_level_key_pattern = re.compile(
-            '\w+\s\w+\=|\w+\s\w+\-\w+|\w+\s\w+\s\=')
+        # second_level_key_pattern = re.compile(
+        #     '\w+\s\w+\=|\w+\s\w+\-\w+|\w+\s\w+\s\=')
         first_level_keys = []
-        second_level_keys_list = []
+        second_level_keys = []
 
         for el in genkey_elements:
             genkey_element = ' '.join([c.strip() for c in el.split(' ')])
             _first_level_key = first_level_key_pattern.search(genkey_element)
 
             if _first_level_key:
-                second_level_keys = second_level_key_pattern.findall(el)
-                second_level_keys = {k.split(' ')[0]
-                                     for k in second_level_keys if k}
                 first_level_key = _first_level_key.group().replace('!', '').strip()
-
                 first_level_keys.append(first_level_key)
-                second_level_keys_list.append(second_level_keys)
+
+                lines = self.clean_lines(el)
+                elements = list(map(self.split_values, lines))
+                second_keys = [el[0] for el in elements]
+                list_values = [el[1:] for el in elements]
+                # breakpoint()
+                values = list(map(self.get_dict_values, list_values))
+                key_vals_list = list(zip(second_keys, values))
+
+                key_vals_dict = {}
+                for key in key_vals_list:
+                    key_vals_dict.setdefault(key[0], []).append(key[1])
+
+                for key, val in key_vals_dict.items():
+                    if len(val) == 1:
+                        key_vals_dict[key] = key_vals_dict.get(key)[0]
+
+                second_level_keys.append(key_vals_dict)
+
+        #     if _first_level_key:
+        #         second_level_keys = second_level_key_pattern.findall(el)
+        #         second_level_keys = {k.split(' ')[0]
+        #                              for k in second_level_keys if k}
+        #         first_level_key = _first_level_key.group().replace('!', '').strip()
+
+        #         first_level_keys.append(first_level_key)
+        #         second_level_keys_list.append(second_level_keys)
 
         # Putting together first and second level keys
-        genkey_keys = list(zip(first_level_keys, second_level_keys_list))
+        genkey_keys = list(zip(first_level_keys, second_level_keys))
 
         # Creating list of second level keys for duplicated first level keys
         for key in genkey_keys:
@@ -657,10 +817,12 @@ class Genkey(dict):
         # Setting None first level key value if its value is empty.
         for key, val in self.items():
             if len(val) == 1:
-                self[key] = self.fromkeys(self.get(key)[0])
+                self[key] = self.get(key)[0]
 
-            if not bool(val[0]):
-                self[key] = None
+            # if not bool(val[0]):
+            #     self[key] = None
+
+        # breakpoint()
 
 
 if __name__ == "__main__":
